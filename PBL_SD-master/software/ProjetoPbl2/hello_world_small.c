708186798193a54ae6d73afccfd1e0d920365e49
Leandro Sampaio <leandro2006-@hotmail.com>
2018-11-22 15:13:31 -0300

    Updates

--- PBL_SD-master/software/ProjetoPbl2/hello_world_small.c
+++ PBL_SD-master/software/ProjetoPbl2/hello_world_small.c
@@ -31,16 +31,41 @@ void write_uart(int tamanho, char *comando)
 
 }
 
+void write_uart_quick(int tamanho, char comando)
+{
+	printf("COMANDO: %c", comando);
+    unsigned long status = 0;
+    int i;
+    // acenderLeds(1, 0);
+    // Verifica até que seja possível a transmissão de dados
+    while((status & 0x00000040) != 0x00000040)
+    {
+        status = IORD_ALTERA_AVALON_UART_STATUS(UART_1_BASE);
+    }
+
+    for (i = 0; i < tamanho; i++) {
+    	IOWR_ALTERA_AVALON_UART_TXDATA(UART_1_BASE, comando);
+    	usleep(1000);
+    }
+
+}
+
 char read_uart()
 {
+    int x=0;
 	char data;
     while(1){
     	if(IORD_ALTERA_AVALON_UART_STATUS( UART_1_BASE ) & 0x80 ) {
     		data = IORD_ALTERA_AVALON_UART_RXDATA( UART_1_BASE );
 	        printf("%c",data);
+
+	        if(data == 'K'){
+	        	break;
+	        }
     	}
 	}
 }
+
 
 void acenderLeds(int opcao, int nivelLogico) {
 	if (opcao == 1) {
@@ -114,7 +139,26 @@ void lcd_init(void) {
 int main() {
 	//printf("Hello from Nioss II!!\n");
 
-	int sw_value, sw_value1, sw_value2, sw_value3;
+	// Alterar o x12 - Tamanho do pacote
+	// O ID= 'Nios 2'
+	char connect[] = {0x10, 0x11, 0x00, 0x04, 0x4D, 0x51, 0x54, 0x54,
+			0x04, 0x02, 0x00, 0x14, 0x00, 0x05, 0x4E, 0x69, 0x6F, 0x73, 0x35};
+
+	// 0x30 -> Código da mensagem para o publish
+	// 0x0F -> Tamanho da mensagem (decimal)
+	// 0x00, 0x02 -> Tamanho do tópico
+	char publish[] = {0x30, 0x18, 0x00, 0x0C,
+			0x74, 0x65, 0x73, 0x74, 0x65, 0x2f, 0x74, 0x65, 0x73, 0x74, 0x65, 0x31,
+			0x48, 0x65, 0x6C, 0x6C, 0x6F, 0x57, 0x6F, 0x72, 0x6C, 0x64};
+	/*char publish[] = {0x30, 0x11, 0x05,
+				0x74, 0x65, 0x73, 0x74, 0x65,
+				0x48, 0x65, 0x6C, 0x6C, 0x6F, 0x57, 0x6F, 0x72, 0x6C, 0x64};*/
+	/*char publish[] = {0x30, 0x0F, 0x00, 0x02,
+					0x53, 0x44, 0x48, 0x65, 0x6C,
+					0x6C, 0x6F, 0x20, 0x57, 0x6F, 0x72, 0x6C, 0x64};*/
+	//char disconnect[] = {0xE0, 0x00};
+
+ 	int sw_value, sw_value1, sw_value2, sw_value3;
 	int opcao = 1;
 	int entrouOpcao = 0, liberado = 1;
 
@@ -141,12 +185,78 @@ int main() {
 
 //_____________________________________________________
 
-	char comando[] = "AT+GMR";
+	int x, z;
+	char comando[] = "AT";
 	char resposta;
 	//printf("%d",strlen(comando));
 	write_uart(strlen(comando), comando);
 	read_uart();
+	char cmd[] = "AT+CWJAP=\"WLessLEDS\",\"HelloWorldMP31\"";
+	//char cmd[] = "AT+CWJAP=\"IEEE_UEFS\",\"ramoIEEEUEFS\"";
+	//char cmd[] = "AT+CWJAP=\"Gilberto WiFi\",\"20152015\"";
+	write_uart(strlen(cmd), cmd);
+	read_uart();
+	printf("CONECTOU A REDE!");
 
+	//192.168.1.100 1883
+	// Conectando ao cloudmqtt
+	char cmd2[] = "AT+CIPSTART=\"TCP\",\"192.168.1.103\",1883";
+	//char cmd2[] = "AT+CIPSTART=\"TCP\",\"test.mosquitto.org\",1883";
+	write_uart(strlen(cmd2), cmd2);
+	read_uart();
+	//printf("%c", connect[0]);
+	printf("CONEXAO TCP!");
+	printf("%d", sizeof(connect));
+	/*
+	char cmd3[] = "AT+CIPSTATUS";
+	write_uart(strlen(cmd3), cmd3);
+	read_uart();
+	*/
+	char cmd3[] = "AT+CIPSEND=19";
+	//char cmd4 = strlen(connect);
+	//write_uart_quick(strlen(cmd3), cmd3);
+	write_uart(strlen(cmd3),cmd3);
+	read_uart();
+	//printf(strlen(connect));
+	//write_uart(strlen(connect),connect);
+	for(z=0; z < sizeof(connect); z++){
+		printf("%c", connect[z]);
+		write_uart_quick(1, connect[z]);
+	}
+	IOWR_ALTERA_AVALON_UART_TXDATA(UART_1_BASE, '\r');
+	usleep(1000);
+	IOWR_ALTERA_AVALON_UART_TXDATA(UART_1_BASE, '\n');
+	read_uart();
+
+	printf("\nCONEXAO COM O BROKER CONCLUIDA!");
+
+	// 26- teste/teste1
+	char cmd4[] = "AT+CIPSEND=26";
+	write_uart(strlen(cmd4),cmd4);
+	//read_uart();
+	printf("\nTAMANHO: %d\n",sizeof(publish));
+	//write_uart(strlen(connect),connect);
+	printf("---------------\n");
+	printf("%c", publish[0]);
+	printf("---------------\n");
+	printf("%c", publish[1]);
+	printf("----------------\n");
+
+	for(z=0; z < sizeof(publish); z++){
+		printf("---------------\n");
+		printf("%d",z);
+		printf(": %c", publish[z]);
+		write_uart_quick(1, publish[z]);
+	}
+
+	IOWR_ALTERA_AVALON_UART_TXDATA(UART_1_BASE, '\r');
+	usleep(1000);
+	IOWR_ALTERA_AVALON_UART_TXDATA(UART_1_BASE, '\n');
+	read_uart();
+
+
+
+	printf("Terminou!");
 
 //_____________________________________________________
 	while (1) {
